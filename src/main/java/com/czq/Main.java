6182b3973f28d7fb5300962f8f01881604ab7811
Jax <352936816@qq.com>
2022-11-15 17:52:37 +0800

    hot-fix hello git1

--- src/main/java/com/czq/Main.java
+++ src/main/java/com/czq/Main.java
@@ -3,5 +3,6 @@ package com.czq;
 public class Main {
     public static void main(String[] args) {
         System.out.println("master hello git1");
+        System.out.println("hot-fix hello git1");
     }
 }
