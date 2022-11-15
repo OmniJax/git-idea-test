300fa6effd0a6431fed677124ba65f15643c6d06
Jax <352936816@qq.com>
2022-11-15 17:56:06 +0800

    master 1 after merge

--- src/main/java/com/czq/Main.java
+++ src/main/java/com/czq/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("master hello git1");
         System.out.println("hot-fix hello git1");
+
+        System.out.println("master 1 after merge");
     }
 }
